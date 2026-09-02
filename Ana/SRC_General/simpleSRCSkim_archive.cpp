537ddbeae5e1f73c216219b518ee9f4408485eeb
Natalie Wright <natalie@getwright.com>
2026-09-02 16:48:50 -0400

    lc

--- Ana/SRC_General/simpleSRCSkim_archive.cpp
+++ Ana/SRC_General/simpleSRCSkim_archive.cpp
@@ -24,27 +24,9 @@ const double mP = 0.938;
 const double mU = 0.9314941024;
 const double me = 0.000511;
 const double m_4He = 4.00260325415 * mU - 2 * me;
-const double m_12C = 12.0 * 0.93149410242 - 6.0 * 0.00051099895;
+const int kTargetMassNumber = 4;
 const double kLightConeEdgeEpsilon = 1e-6;
 const double kLightConeRoundoffTolerance = 1e-9;
-
-struct TargetConfig {
-  const char *name;
-  double mass;
-  int massNumber;
-  int Z;
-  int N;
-  double sigmaCM;
-};
-
-TargetConfig parseTargetConfig(const char *arg)
-{
-  const std::string target = arg ? arg : "";
-  if(target == "c12" || target == "C12" || target == "carbon12" || target == "carbon-12") {
-    return {"C12", m_12C, 12, 6, 6, 0.15};
-  }
-  return {"He4", m_4He, 4, 2, 2, 0.10};
-}
 
 const int MAXP = 4;  // max number of proton candidates considered per event
 
@@ -286,7 +268,7 @@ void runLightConeSelfTests()
 
 void Usage()
 {
-  cerr << "Usage: ./code <MC=1,Data=0> <Ebeam(GeV)> <output.root> [he4|c12] <input.hipo> [more hipo...]\n";
+  cerr << "Usage: ./code <MC=1,Data=0> <Ebeam(GeV)> <output.root> <input.hipo> [more hipo...]\n";
 }
 
 bool inFD(int status){ return (abs(status) >= 2000 && abs(status) < 4000); }
